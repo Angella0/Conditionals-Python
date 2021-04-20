dcc5e9e5153b5194422a3049c440af860c2fd459
Angella0 <angellasimbwa@gmail.com>
2021-04-20 09:07:53 +0300

    if statements

---
+++ main.py
@@ -0,0 +1,22 @@
+# if statements
+is_male = False
+is_tall = False
+
+if is_male and is_tall:
+    print("you are a  tall male")
+elif is_male and not(is_tall):
+    print("you are a short male")
+elif not (is_male) and not is_tall:
+    print("you are not a male but tall")
+else:
+    print("you are either not male or not tall or both")
+#comparison operators
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(300,40,5))
